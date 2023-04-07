8290fe1f84142fcb21b2c215ae4661994765213d
lilia <lilianom59@outlook.fr>
2023-04-07 14:20:48 +0200

    test kaernunos

---
+++ navbar.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>test kaernunos</title>
+</head>
+<body>
+<nav class="navbar">
+    <div class="nav-links">
+        <a href="#" class="logo">KAERNUNOS</a>
+        <ul>
+            <li class="active"><a href="#">Univers</a></li>
+            <li><a href="#">Animation</a></li>
+            <li><a href="#">Info & Contact</a></li>
+            <li><a href="#">A propos</a></li>
+        </ul>
+    </div>
+    <a href="#" class="login">Se connecter</a>
+</nav>
+</body>
+</html>
+
+
+<?php
